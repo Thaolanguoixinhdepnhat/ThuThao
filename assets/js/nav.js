6d1d8f9dfbbbfe641e62aa1cb05b3c4189944ff2
Thaone <hungtuky2003@gmail.com>
2025-04-28 14:00:57 +0700

    update

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.querySelector('.menu-icon'); // Biểu tượng menu
-    const nav = document.querySelector('.header .nav'); // Menu chính
-    const contactBtn = document.querySelector('.nav .contact-btn'); // Nút liên hệ
+$(document).ready(function() {
+    const $menuIcon = $('.menu-icon'); // Biểu tượng menu
+    const $nav = $('.header .nav'); // Menu chính
+    const $contactBtn = $('.nav .contact-btn'); // Nút liên hệ
 
-    menuIcon.addEventListener('click', () => {
-        nav.classList.toggle('open'); // Hiển thị menu
-        contactBtn.classList.toggle('open'); // Hiển thị nút liên hệ
-        menuIcon.classList.toggle('open'); // Hiệu ứng cho biểu tượng menu
+    $menuIcon.on('click', function() {
+        $nav.toggleClass('open'); // Hiển thị menu
+        $contactBtn.toggleClass('open'); // Hiển thị nút liên hệ
+        $menuIcon.toggleClass('open'); // Hiệu ứng cho biểu tượng menu
     });
 });
